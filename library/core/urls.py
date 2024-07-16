18dd9bc61071667ca622d9cc6749561a47c6b114
Conrad Shock <shockconrad@gmail.com>
2024-07-16 14:30:51 -0700

    feat: set library member perm group on signup

--- library/core/urls.py
+++ library/core/urls.py
@@ -1,5 +1,8 @@
 from django.urls import include, path
 
+from .views import LibraryMemberSignupView
+
 urlpatterns = [
+    path("accounts/signup/", LibraryMemberSignupView.as_view(), name="account_signup"),
     path("accounts/", include("allauth.urls")),
 ]
--- library/core/views.py
+++ library/core/views.py
@@ -1,1 +1,20 @@
-# Create your views here.
+from typing import override
+
+from allauth.account.forms import SignupForm
+from allauth.account.views import SignupView
+from django.contrib.auth.models import Group
+
+from .models import User
+
+
+class LibraryMemberSignupView(SignupView):
+    class LibraryMemberSignupForm(SignupForm):
+        @override
+        def save(self, request):
+            user: User = super().save(request)
+            # gets users the correct permissions on signup
+            user.groups.add(Group.objects.get(name="Library Members"))
+            user.save()
+            return user
+
+    form_class = LibraryMemberSignupForm
